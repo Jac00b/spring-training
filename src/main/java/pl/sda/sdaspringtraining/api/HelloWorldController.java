0e19c7f60468b8f664a41044b017f0a06a4cb917
Jakub <jakub.misiuk@gmail.com>
2020-04-25 11:53:06 +0200

    RequestParams

--- src/main/java/pl/sda/sdaspringtraining/api/HelloWorldController.java
+++ src/main/java/pl/sda/sdaspringtraining/api/HelloWorldController.java
@@ -1,6 +1,8 @@
 package pl.sda.sdaspringtraining.api;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,6 +13,14 @@ public class HelloWorldController {
         return "Hello world";
     }
 
+    @GetMapping("/hello/{name}")
+    String helloWorldWithName(@PathVariable("name") String name){
+        return "Hello world, " +name;
+    }
 
+    @GetMapping("/multiply")
+    String multiply(@RequestParam("f1") Integer factorOne, @RequestParam("f2") Integer factorTwo) {
+        return "Wynik mnożenia to : " + (factorOne * factorTwo);
+    }
 
 }
